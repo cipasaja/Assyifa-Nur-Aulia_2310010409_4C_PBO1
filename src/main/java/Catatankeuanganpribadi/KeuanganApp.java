af793c1a398646d6b7b52e4bdc5974afb80bbec1
assyifa nur aulia <assyifanuraulia12@gmail.com>
2025-07-08 16:14:23 +0800

    Menambahkan list transaksi dan konstruktor

--- src/main/java/Catatankeuanganpribadi/KeuanganApp.java
+++ src/main/java/Catatankeuanganpribadi/KeuanganApp.java
@@ -1,13 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Catatankeuanganpribadi;
 
-/**
- *
- * @author WIN 11
- */
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class KeuanganApp {
-    
+
+    private final List<Transaksi> transaksiList;
+
+    public KeuanganApp() {
+        transaksiList = new ArrayList<>();
+    }
 }
