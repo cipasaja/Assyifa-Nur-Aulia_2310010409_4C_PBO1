1cb8f3d751570c9210c7299e03a7d3b84468ddb0
assyifa nur aulia <assyifanuraulia12@gmail.com>
2025-07-08 15:51:28 +0800

    Menambahkan konstruktor dengan parameter lengkap

--- src/main/java/Catatankeuanganpribadi/Pengeluaran.java
+++ src/main/java/Catatankeuanganpribadi/Pengeluaran.java
@@ -5,4 +5,9 @@ import java.time.LocalDate;
 public class Pengeluaran extends Transaksi {
     private String metodePembayaran;
 
+    public Pengeluaran(LocalDate tanggal, double nominal, String kategori, String keterangan, String metodePembayaran) {
+    super(tanggal, nominal, kategori, keterangan);
+    this.metodePembayaran = metodePembayaran;
+}
+
 }
