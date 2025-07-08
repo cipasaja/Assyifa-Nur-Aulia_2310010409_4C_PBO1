3f6e104468f466e7e9ff31055f62016b1bfe6d5b
assyifa nur aulia <assyifanuraulia12@gmail.com>
2025-07-08 15:59:55 +0800

    Menambahkan konstruktor dengan parameter lengkap termasuk sumber

--- src/main/java/Catatankeuanganpribadi/Pemasukan.java
+++ src/main/java/Catatankeuanganpribadi/Pemasukan.java
@@ -5,4 +5,9 @@ import java.time.LocalDate;
 public class Pemasukan {
     private String sumber;
 
+    public Pemasukan(LocalDate tanggal, double nominal, String kategori, String keterangan, String sumber) {
+        super(tanggal, nominal, kategori, keterangan);
+        this.sumber = sumber;
+}
+
 }
