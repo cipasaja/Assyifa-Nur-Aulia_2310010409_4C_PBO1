a51d532fdab3698c2fbb38268d7dc88c922e1b3e
assyifa nur aulia <assyifanuraulia12@gmail.com>
2025-07-08 16:00:53 +0800

    Menambahkan getter dan setter

--- src/main/java/Catatankeuanganpribadi/Pemasukan.java
+++ src/main/java/Catatankeuanganpribadi/Pemasukan.java
@@ -9,5 +9,8 @@ public class Pemasukan {
         super(tanggal, nominal, kategori, keterangan);
         this.sumber = sumber;
 }
+    public String getSumber() { return sumber; }
+    public void setSumber(String sumber) { this.sumber = sumber; }
+
 
 }
