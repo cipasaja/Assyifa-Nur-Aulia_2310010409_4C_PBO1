206e8f38f2fd03db0592ffd2752d351755f4a69b
assyifa nur aulia <assyifanuraulia12@gmail.com>
2025-07-08 16:18:48 +0800

    Menambahkan method main

--- src/main/java/Catatankeuanganpribadi/KeuanganApp.java
+++ src/main/java/Catatankeuanganpribadi/KeuanganApp.java
@@ -39,5 +39,56 @@ public class KeuanganApp {
     }
     return saldo;
     }
+    public static void main(String[] args) {
+        KeuanganApp app = new KeuanganApp();
+        Scanner sc = new Scanner(System.in);
+        int pilih;
+        do {
+            System.out.println("=== MENU CATATAN KEUANGAN ===");
+            System.out.println("1. Tambah Pemasukan");
+            System.out.println("2. Tambah Pengeluaran");
+            System.out.println("3. Tampilkan Semua Transaksi");
+            System.out.println("4. Hitung Saldo");
+            System.out.println("0. Keluar");
+            System.out.print("Pilih: ");
+            pilih = sc.nextInt();
+            sc.nextLine();
+
+            switch (pilih) {
+                case 1 -> {
+                    System.out.print("Tanggal (dd/mm/yyyy): ");
+                    LocalDate tgl = LocalDate.parse(sc.nextLine(), Transaksi.TANGGAL_FMT);
+                    System.out.print("Nominal: ");
+                    double nominal = sc.nextDouble(); sc.nextLine();
+                    System.out.print("Kategori: ");
+                    String kategori = sc.nextLine();
+                    System.out.print("Keterangan: ");
+                    String ket = sc.nextLine();
+                    System.out.print("Sumber pemasukan: ");
+                    String sumber = sc.nextLine();
+                    app.tambahTransaksi(new Pemasukan(tgl, nominal, kategori, ket, sumber));
+                }
+                case 2 -> {
+                    System.out.print("Tanggal (dd/mm/yyyy): ");
+                    LocalDate tgl = LocalDate.parse(sc.nextLine(), Transaksi.TANGGAL_FMT);
+                    System.out.print("Nominal: ");
+                    double nominal = sc.nextDouble(); sc.nextLine();
+                    System.out.print("Kategori: ");
+                    String kategori = sc.nextLine();
+                    System.out.print("Keterangan: ");
+                    String ket = sc.nextLine();
+                    System.out.print("Metode pembayaran: ");
+                    String metode = sc.nextLine();
+                    app.tambahTransaksi(new Pengeluaran(tgl, nominal, kategori, ket, metode));
+                }
+                case 3 -> app.tampilkanSemua();
+                case 4 -> System.out.printf("Saldo anda saat ini: Rp%,.2f\n\n", app.hitungSaldo());
+                case 0 -> System.out.println("Terima kasih! Program selesai.");
+                default -> System.out.println("Pilihan tidak valid.\n");
+            }
+        } while (pilih != 0);
+
+        sc.close();
+    }
 
 }
