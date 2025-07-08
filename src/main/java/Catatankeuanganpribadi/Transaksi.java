bf6f186f849517ca0abe9132f333237390959e13
assyifa nur aulia <assyifanuraulia12@gmail.com>
2025-07-08 15:38:30 +0800

    Menambahkan getter dan setter untuk semua atribut

--- src/main/java/Catatankeuanganpribadi/Transaksi.java
+++ src/main/java/Catatankeuanganpribadi/Transaksi.java
@@ -14,6 +14,18 @@ public Transaksi(LocalDate tanggal, double nominal, String kategori, String kete
     this.kategori = kategori;
     this.keterangan = keterangan;
 }
+public LocalDate getTanggal() { return tanggal; }
+public void setTanggal(LocalDate tanggal) { this.tanggal = tanggal; }
+
+public double getNominal() { return nominal; }
+public void setNominal(double nominal) { this.nominal = nominal; }
+
+public String getKategori() { return kategori; }
+public void setKategori(String kategori) { this.kategori = kategori; }
+
+public String getKeterangan() { return keterangan; }
+public void setKeterangan(String keterangan) { this.keterangan = keterangan; }
+
 }
 
 
